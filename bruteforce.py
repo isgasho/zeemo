1fee593288b22e3095af89780030e4a0a46735b6
Zack Scholl <zack.scholl@gmail.com>
2023-04-13 07:55:30 -0700

    add bruteforce

--- bruteforce.py
+++ bruteforce.py
@@ -1,11 +1,48 @@
+resistors = [
+    560,
+    510,
+    180,
+    820,
+    20,
+    22,
+    36,
+    33,
+    120,
+    470,
+    430,
+    750,
+    680,
+    24,
+    39,
+    43,
+    200,
+    750,
+    620,
+    330,
+    390,
+    51,
+    220,
+    270,
+    910,
+    47,
+    470,
+    750,
+    75,
+    100,
+    63,
+    27,
+    30,
+]
+
+
 def find_vals(vwant):
     # add tolerance
     diff = 1000
     vals = []
     vin = [0, 3.3]
-    for rf in range(1, 150):
-        for r1 in range(1, 150):
-            for r2 in range(1, 150):
+    for _, rf in enumerate(resistors):
+        for _, r1 in enumerate(resistors):
+            for _, r2 in enumerate(resistors):
                 vout = [
                     -1 * rf * (vin[1] / r1 - 10 / r2),
                     -1 * rf * (vin[0] / r1 - 10 / r2),
