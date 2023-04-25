522b01dc480eb2ca5d6c521d811fb32f08147541
Zack <zack.scholl@gmail.com>
2023-04-24 19:01:53 -0700

    update naming

---
+++ pico/code.py
@@ -0,0 +1,49 @@
+import time
+import usb_midi
+import adafruit_midi
+from adafruit_midi.control_change import ControlChange
+import supervisor
+
+supervisor.set_usb_identification("ZEEMO")
+
+from machine import Pin, PWM
+
+# overclocking may improve pwm performance?
+# import machine
+# machine.freq(270000000)
+
+# LUA CODE FOR UNPACKING
+# function unpack_14bit_to_7bit(num)
+#   local highBits = num >> 7 -- shift right by 7 bits to get the high bits
+#   local lowBits = num & 0x7F -- bitwise AND with 0x7F (01111111 in binary) to get the low bits
+#   return lowBits, highBits
+# end
+# print(split14BitNumber(10389))
+# 21, 81
+
+def pack_7bit_to_14bit(tup) :
+    return ((tup[1]<<7)|tup[0])
+# print(pack_7bit_to_14bit((21,81)))
+# 10389
+
+# setup PWMs
+pwms=[]
+for i, pin in enumerate([6,7,8,9,10,11,12,13]):
+    pwms.append(PWM(pin))
+    # set to low freq, to allow best resolution
+    # https://www.i-programmer.info/programming/hardware/14751-the-pico-in-micropython-pwm.html?start=2
+    pwms[i].freq(round(machine.freq()/16384)) # target 14-bit resolution frequency  
+     # set to 50% duty on start 
+    pwms[i].duty_u16(32768)
+
+
+midi = adafruit_midi.MIDI(midi_in=usb_midi.ports[0])
+while True:
+    time.sleep(0.05)
+    msg = midi.receive()
+    if msg is not None:
+        print("Received:", msg, "at", time.monotonic())
+        # check midi CC
+
+
+
